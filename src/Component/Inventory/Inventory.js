2b4f28d6abe674898d954d1248cb1cfce09f7d49
unknown <ahalya495@gmail.com>
2024-07-23 22:21:15 +0530

    refactor: updated style in nav bar

--- src/Component/Inventory/Inventory.js
+++ src/Component/Inventory/Inventory.js
@@ -1,10 +1,8 @@
 import './Inventory.css';
-
-import {Button, IconButton, Pagination, Paper, Switch, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from '@mui/material';
-import React,{useEffect, useState} from 'react'
-
+import { Button, IconButton, Pagination, Paper, Switch, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 import AssignmentTurnedInOutlinedIcon from '@mui/icons-material/AssignmentTurnedInOutlined';
-import BorderColorOutlinedIcon from '@mui/icons-material/BorderColorOutlined'
+import BorderColorOutlinedIcon from '@mui/icons-material/BorderColorOutlined';
 import ControlPointSharpIcon from '@mui/icons-material/ControlPointSharp';
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import EditModal from '../EditModal/EditModal';
@@ -16,21 +14,10 @@ function Inventory() {
     const [page, setPage] = useState(1);
     const [open, setOpen] = useState(false);
     const [selectedItem, setSelectedItem] = useState(null);
-    
-    let history=useNavigate();
     const itemsPerPage = 6;
+    const history = useNavigate();
 
-    const displayItems = stock.slice((page - 1) * itemsPerPage, page * itemsPerPage);
-    
-    const addproduct=()=>{
-        let path='/AdminDashboard';
-        history(path);
-    };
-    const mailvendor=()=>{
-        let path='/MailVendor';
-        history(path);
-    }
-    
+    // Function to fetch stock data based on page number
     const loadStock = async () => {
         try {
             const data = await fetchStock();
@@ -39,122 +26,127 @@ function Inventory() {
             console.error('Error loading stock data:', error);
         }
     };
-    
+
     useEffect(() => {
         loadStock();
-    }, []);
-    
-    
-    
+    }, [page]); 
+
+  
+    const displayItems = stock.slice((page - 1) * itemsPerPage, page * itemsPerPage);
+
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
     };
-    
+
     const handleOpenModal = (item) => {
         setSelectedItem(item);
         setOpen(true);
-      };
-    
-      const handleCloseModal = () => {
+    };
+
+    const handleCloseModal = () => {
         setOpen(false);
         setSelectedItem(null);
-        loadStock();
-        
-      };
-      
-      const handleStockDelete = async (item) => {
+        loadStock(); 
+    };
+
+    const handleStockDelete = async (item) => {
         try {
             const response = await deleteStock(item?.Name);
-             if (response.ok) {
-               console.log("Stock deleted SuccessFully");
-               loadStock();
-             } else {
-               console.error("Error in deletion");
-             }
-           } catch (error) {
-             console.error('Error deleting Stock:', error);
-           }
-      };
+            if (response.ok) {
+                console.log("Stock deleted successfully");
+                loadStock(); // Reload stock after deletion
+            } else {
+                console.error("Error in deletion");
+            }
+        } catch (error) {
+            console.error('Error deleting stock:', error);
+        }
+    };
 
-  return (
-    <div>
-        <div className='button_add'>
-        <Button variant='outlined' endIcon={<AssignmentTurnedInOutlinedIcon/>} sx={{backgroundColor:'#17BBC0',color:'#FFFFFF',borderColor:'#333333',':hover': {
-                        backgroundColor: '#17BBC0',
-                        color: '#FFFFFF', borderColor: '#333333'
-                    }}} onClick={mailvendor}>PLACE ORDER</Button>
-        <Button variant='outlined' endIcon={<ControlPointSharpIcon/>} sx={{backgroundColor:'#17BBC0',color:'#FFFFFF',borderColor:'#333333',':hover': {
-                        backgroundColor: '#17BBC0',
-                        color: '#FFFFFF',borderColor:'#333333'
-                    }}} onClick={addproduct}>ADD NEW PRODUCT</Button></div>
+    const addProduct = () => {
+        let path = '/AdminDashboard';
+        history(path);
+    };
 
-        <TableContainer component={Paper} sx={{ margin: '20px', backgroundColor: '#2c2c2c', color: '#fff' ,borderRadius:'10px'}}>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell sx={{ color: '#fff' }}>Name</TableCell>
-                        <TableCell sx={{ color: '#fff' }}>ID</TableCell>
-                        <TableCell sx={{ color: '#fff' }}>Category</TableCell>
-                        <TableCell sx={{ color: '#fff' }}>Quantity</TableCell>
-                        <TableCell sx={{ color: '#fff' }}>Availability</TableCell>
-                        <TableCell sx={{ color: '#fff' }}>Price/Unit</TableCell>
-                        <TableCell sx={{ color: '#fff' }}>Action</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {displayItems.map((item) => (
-                        <TableRow key={item.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                            <TableCell sx={{ color: '#fff' }}>{item.Name}</TableCell>
-                            <TableCell sx={{ color: '#fff' }}>{item.id}</TableCell>
-                            <TableCell sx={{ color: '#fff' }}>{item.Category}</TableCell>
-                            <TableCell sx={{ color: '#fff' }}>{item.Quantity}/1000</TableCell>
-                            <TableCell>
-                                <Switch checked={item.Quantity > 0}  sx={{
-                                            '& .MuiSwitch-switchBase.Mui-checked': {
-                                                color: '#6FCF97',
-                                            },
-                                            '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
-                                                backgroundColor: '#6FCF97',
-                                            },}}/>
-                            </TableCell>
-                            <TableCell sx={{ color: '#fff' }}>₹ {item.Price}</TableCell>
-                            <TableCell>
-                                <IconButton onClick={() => handleOpenModal(item)}>
-                                    <BorderColorOutlinedIcon sx={{ color: '#568D76' }}/>
-                                </IconButton>
-                                <IconButton onClick={()=>handleStockDelete(item)}>
-                                    <DeleteOutlineOutlinedIcon  sx={{ color: '#C75D7A' }}  />
-                                </IconButton>
-                            </TableCell>
+    const mailVendor = () => {
+        let path = '/MailVendor';
+        history(path);
+    };
+
+    return (
+        <div>
+            <div className='button_add'>
+                <Button variant='outlined' endIcon={<AssignmentTurnedInOutlinedIcon />} sx={{ backgroundColor: '#17BBC0', color: '#FFFFFF', borderColor: '#333333', ':hover': { backgroundColor: '#17BBC0', color: '#FFFFFF', borderColor: '#333333' } }} onClick={mailVendor}>PLACE ORDER</Button>
+                <Button variant='outlined' endIcon={<ControlPointSharpIcon />} sx={{ backgroundColor: '#17BBC0', color: '#FFFFFF', borderColor: '#333333', ':hover': { backgroundColor: '#17BBC0', color: '#FFFFFF', borderColor: '#333333' } }} onClick={addProduct}>ADD NEW PRODUCT</Button>
+            </div>
+
+            <TableContainer component={Paper} sx={{ margin: '20px', backgroundColor: '#2c2c2c', color: '#fff', borderRadius: '10px' }}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell sx={{ color: '#fff' }}>Name</TableCell>
+                            <TableCell sx={{ color: '#fff' }}>ID</TableCell>
+                            <TableCell sx={{ color: '#fff' }}>Category</TableCell>
+                            <TableCell sx={{ color: '#fff' }}>Quantity</TableCell>
+                            <TableCell sx={{ color: '#fff' }}>Availability</TableCell>
+                            <TableCell sx={{ color: '#fff' }}>Price/Unit</TableCell>
+                            <TableCell sx={{ color: '#fff' }}>Action</TableCell>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-            <Pagination
-                count={Math.ceil(stock.length / itemsPerPage)}
-                page={page}
-                onChange={handleChangePage}
-                variant="outlined"
-                shape="rounded"
-                sx={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    marginTop: 2,
-                    '& .MuiPaginationItem-root': {
-                        color: '#FFFF',
-                        backgroundColor: '#474859',
-                        '&:hover': {
-                            backgroundColor: '#FFFF',
-                            color: '#000000',
+                    </TableHead>
+                    <TableBody>
+                        {displayItems.map((item) => (
+                            <TableRow key={item.id} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                <TableCell sx={{ color: '#fff' }}>{item.Name}</TableCell>
+                                <TableCell sx={{ color: '#fff' }}>{item.id}</TableCell>
+                                <TableCell sx={{ color: '#fff' }}>{item.Category}</TableCell>
+                                <TableCell sx={{ color: '#fff' }}>{item.Quantity}/1000</TableCell>
+                                <TableCell>
+                                    <Switch checked={item.Quantity > 0} sx={{
+                                        '& .MuiSwitch-switchBase.Mui-checked': {
+                                            color: '#6FCF97',
+                                        },
+                                        '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
+                                            backgroundColor: '#6FCF97',
+                                        },
+                                    }} />
+                                </TableCell>
+                                <TableCell sx={{ color: '#fff' }}>₹ {item.Price}</TableCell>
+                                <TableCell>
+                                    <IconButton onClick={() => handleOpenModal(item)}>
+                                        <BorderColorOutlinedIcon sx={{ color: '#568D76' }} />
+                                    </IconButton>
+                                    <IconButton onClick={() => handleStockDelete(item)}>
+                                        <DeleteOutlineOutlinedIcon sx={{ color: '#C75D7A' }} />
+                                    </IconButton>
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+                <Pagination
+                    count={Math.ceil(stock.length / itemsPerPage)}
+                    page={page}
+                    onChange={handleChangePage}
+                    variant="outlined"
+                    shape="rounded"
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'center',
+                        marginTop: 2,
+                        '& .MuiPaginationItem-root': {
+                            color: '#FFFF',
+                            backgroundColor: '#474859',
+                            '&:hover': {
+                                backgroundColor: '#FFFF',
+                                color: '#000000',
+                            },
                         },
-                    },
-                }}
-            />
-        
-        </TableContainer>
-        <EditModal open={open} handleClose={handleCloseModal} item={selectedItem} />
-         </div>
-  )
+                    }}
+                />
+            </TableContainer>
+            <EditModal open={open} handleClose={handleCloseModal} item={selectedItem} />
+        </div>
+    )
 }
 
-export default Inventory
+export default Inventory;
--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,48 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import InventoryIcon from '@mui/icons-material/Inventory';
 import SettingsIcon from '@mui/icons-material/Settings';
 import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
-import Logo from '../../Asset/Ls.png'
-import {useNavigate} from "react-router-dom";
-import './Navbar.css'
-import DashBoard from '../DashBoard/DashBoard';
-import AdminDashBoard from '../AdminDashboard/AdminDashBoard';
-function Navbar() {
-    const history=useNavigate();
-    const Inventory=()=>{
-        let path='/Inventory';
-        history(path);
-    }
-    const DashBoard=()=>{
-        let path='/';
-        history(path);
-    }
-    const AdminDashBoard=()=>{
-        let path='/AdminDashboard';
-        history(path);
-    }
+import Logo from '../../Asset/Ls.png';
+import { useNavigate } from 'react-router-dom';
+import './Navbar.css';
 
-  return (
-    <div className='navbar'>
-        <div className="logo_div">
-        <img src={Logo} className='logo'/></div>
-        <div className='nav_items'>
-        <div className='nav_item'>
-        <DashboardIcon className='icon' sx={{fontSize:'35px'}} onClick={DashBoard}/>
-        <span>DashBoard</span></div>
-        <div className='nav_item'>
-        <InventoryIcon className='icon' sx={{fontSize:'35px'}} onClick={Inventory}/>
-        <span>Inventory</span></div>
-        <div className='nav_item'>
-        <SettingsIcon className="icon" sx={{fontSize:'35px'}}/>
-        <span>Settings</span></div>
-        <div className='nav_item'>
-        <AdminPanelSettingsIcon className='icon' sx={{fontSize:'35px'}} onClick={AdminDashBoard}/>
-        <span>Admin</span></div>
+function Navbar() {
+    const history = useNavigate();
+    const [activeItem, setActiveItem] = useState('/');
+
+    const handleItemClick = (path) => {
+        history(path);
+        setActiveItem(path);
+    };
+
+    return (
+        <div className="navbar">
+            <div className="logo_div">
+                <img src={Logo} className="logo" alt="logo_image" />
+            </div>
+            <div className="nav_items">
+                <div className={`nav_item ${activeItem === '/' ? 'active' : ''}`} onClick={() => handleItemClick('/')}>
+                    <DashboardIcon className="icon" sx={{ fontSize: '35px' }} />
+                    <span>Dashboard</span>
+                </div>
+                <div className={`nav_item ${activeItem === '/Inventory' ? 'active' : ''}`} onClick={() => handleItemClick('/Inventory')}>
+                    <InventoryIcon className="icon" sx={{ fontSize: '35px' }} />
+                    <span>Inventory</span>
+                </div>
+                <div className="nav_item">
+                    <SettingsIcon className="icon" sx={{ fontSize: '35px' }} />
+                    <span>Settings</span>
+                </div>
+                <div className={`nav_item ${activeItem === '/AdminDashboard' ? 'active' : ''}`} onClick={() => handleItemClick('/AdminDashboard')}>
+                    <AdminPanelSettingsIcon className="icon" sx={{ fontSize: '35px' }} />
+                    <span>Admin</span>
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    );
 }
 
-export default Navbar
+export default Navbar;
